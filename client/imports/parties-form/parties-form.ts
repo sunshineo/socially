3cbe1cf933b1a63e3549b045a278fb067a3c07a8
Gordon Sun <ssun@zulily.com>
2016-05-25 14:04:58 -0700

    Finish step 8.

--- client/imports/parties-form/parties-form.ts
+++ client/imports/parties-form/parties-form.ts
@@ -2,35 +2,45 @@ import 'reflect-metadata';
 import { Component } from '@angular/core';
 import { FormBuilder, ControlGroup, Validators, Control } from '@angular/common';
 import { Parties } from '../../../collections/parties.ts';
+import { Meteor } from 'meteor/meteor';
+import {MeteorComponent} from 'angular2-meteor';
+import { InjectUser } from 'angular2-meteor-accounts-ui';
  
 @Component({
   selector: 'parties-form',
   templateUrl: '/client/imports/parties-form/parties-form.html'
 })
-export class PartiesForm { 
+@InjectUser("user")
+export class PartiesForm extends MeteorComponent { 
 	partiesForm: ControlGroup;
 
+	user: Meteor.User
 	constructor() {
+		super();
 		let fb = new FormBuilder();
-
 		this.partiesForm = fb.group({
 			name: ['', Validators.required],
 			description: [''],
 			location: ['', Validators.required]
 		});
+		console.log(this.user);
 	}
 
 	addParty(party) {
-		if (this.partiesForm.valid) {
+		console.log(this.user);
+		if (Meteor.userId()) {
 			Parties.insert({
 				name: party.name,
 				description: party.description,
-				location: party.location
+				location: party.location,
+				owner: Meteor.userId()
 			});
 
 			(<Control>this.partiesForm.controls['name']).updateValue('');
 			(<Control>this.partiesForm.controls['description']).updateValue('');
 			(<Control>this.partiesForm.controls['location']).updateValue('');
+		} else {
+			alert('Please log in to add a party');
 		}
 	}
 }
--- client/imports/party-details/party-details.ts
+++ client/imports/party-details/party-details.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { RouteParams, RouterLink } from '@angular/router-deprecated';
+import { RouteParams, RouterLink, CanActivate, ComponentInstruction } from '@angular/router-deprecated';
 import {Parties} from '../../../collections/parties.ts';
+import { Meteor } from 'meteor/meteor';
+import { RequireUser } from 'angular2-meteor-accounts-ui';
  
+function checkPermissions(instruction: ComponentInstruction) {
+  var partyId = instruction.params['partyId'];
+  var party = Parties.findOne(partyId);
+  return (party && party.owner == Meteor.userId());
+}
+
 @Component({
   selector: 'party-details',
-  templateUrl: '/client/imports/party-details/party-details.html',
+  templateUrl: 'client/imports/party-details/party-details.html',
   directives: [RouterLink]
 })
+@CanActivate(checkPermissions)
 export class PartyDetails {
   party: Party;
   
@@ -15,12 +24,16 @@ export class PartyDetails {
     this.party = Parties.findOne(partyId);
   }
   saveParty(party) {
-	  Parties.update(party._id, {
-		  $set: {
-			  name: party.name,
-			  description: party.description,
-			  location: party.location
-		  }
-	  });
+	  if (Meteor.userId()) {
+      Parties.update(party._id, {
+        $set: {
+          name: party.name,
+          description: party.description,
+          location: party.location
+        }
+      });
+    } else {
+      alert('Please log in to change this party');
+    }
   }
 }
