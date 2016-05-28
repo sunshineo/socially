0fed9db796e3a702406315d0e0cb7e1a27998f8f
Gordon Sun <sunshineo@gmail.com>
2016-05-27 22:46:46 -0700

    temp

--- client/imports/parties-form/parties-form.ts
+++ client/imports/parties-form/parties-form.ts
@@ -21,7 +21,8 @@ export class PartiesForm extends MeteorComponent {
 		this.partiesForm = fb.group({
 			name: ['', Validators.required],
 			description: [''],
-			location: ['', Validators.required]
+			location: ['', Validators.required],
+            'public': [false]
 		});
 		console.log(this.user);
 	}
@@ -33,12 +34,14 @@ export class PartiesForm extends MeteorComponent {
 				name: party.name,
 				description: party.description,
 				location: party.location,
+                'public': party.public,
 				owner: Meteor.userId()
 			});
 
 			(<Control>this.partiesForm.controls['name']).updateValue('');
 			(<Control>this.partiesForm.controls['description']).updateValue('');
 			(<Control>this.partiesForm.controls['location']).updateValue('');
+            (<Control>this.partiesForm.controls['public']).updateValue(false);
 		} else {
 			alert('Please log in to add a party');
 		}
---
+++ server/parties.ts
@@ -0,0 +1,16 @@
+import {Parties} from '../collections/parties';
+import {Meteor} from 'meteor/meteor';
+ 
+Meteor.publish('parties', function() {
+  return Parties.find({
+    $or: [
+      { 'public': true },
+      {
+        $and: [
+          { owner: this.userId },
+          { owner: { $exists: true } }
+        ]
+      }
+    ]
+  });
+});
