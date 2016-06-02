f6afa3d90e04ed736142eb98f586e2f274c3340b
Gordon Sun <ssun@zulily.com>
2016-06-02 16:52:00 -0700

    Fix the problem.

--- client/imports/party-details/party-details.ts
+++ client/imports/party-details/party-details.ts
@@ -37,7 +37,7 @@ export class PartyDetails extends MeteorComponent {
         this.party = Parties.findOne(partyId);
         this.getUsers(this.party);
       },   true);
-    }, true);
+    });
     this.subscribe('uninvited', partyId, () => {
       this.getUsers(this.party);
     }, true);
